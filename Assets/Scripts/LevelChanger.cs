dfb6630bcdadae539ede792660f4dd5a36fc2510
mik4041 <mik4041@gmail.com>
2014-01-28 23:08:13 +0200

    added level changing

---
+++ Assets/Scripts/LevelChanger.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//small scrip for level changing by "level_name" parameter
+public class LevelChanger : MonoBehaviour {
+
+	public string level_name;
+
+	void OnTriggerStay2D(Collider2D other) {
+		if(other.tag == "Player") {
+			if (Input.GetButton("Vertical")) {
+				Application.LoadLevel(level_name);
+			}
+		}
+	}
+
+}
